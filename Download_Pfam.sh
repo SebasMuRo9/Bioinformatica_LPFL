4288f4272953f8e537af336a87fae51b5e4e482d
SebasMR <sebastian.mr1298@gmail.com>
2026-06-05 04:58:00 -0500

    Actualiza script para descargar Pfam 38.2

--- Download_Pfam.sh
+++ Download_Pfam.sh
@@ -1,6 +1,5 @@
-mkdir pfam
-cd pfam
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam35.0/Pfam-A.full.uniprot.gz
-wget ftp://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam35.0/Pfam-A.full.uniprot.gz.md5
-md5sum -c Pfam-A.full.uniprot.gz.md5
-gunzip Pfam-A.full.uniprot.gz
+mkdir pfam #make directory, crea una carpeta llamada pfam
+cd pfam #entra a la carpeta pfam
+wget https://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam38.2/Pfam-A.hmm.gz #descarga el archivo Pfam-A.hmm.gz desde el servidor FTP de EBI
+gunzip Pfam-A.hmm.gz #descomprime el archivo Pfam-A.hmm.gz
+hmmpress Pfam-A.hmm #prepara el archivo Pfam-A.hmm para su uso con HMMER, creando los archivos necesarios para la búsqueda de perfiles de HMM
