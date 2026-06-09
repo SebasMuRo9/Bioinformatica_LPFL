af583fba0816779c33113d6ba1bf94cbd08ea919
SebasMR <sebastian.mr1298@gmail.com>
2026-06-09 16:43:07 -0500

    Agrega archivo de familias Pfam / Agrega archivo de secuencias UniProt / Agrega archivos .txt

--- Download_Pfam.sh
+++ Download_Pfam.sh
@@ -1,5 +1,17 @@
-mkdir pfam #make directory, crea una carpeta llamada pfam
+
+sudo apt update #actualiza la lista de paquetes disponibles en el sistema
+sudo apt install hmmer -y #instala el software HMMER, que se utiliza para buscar
+
+mkdir -p pfam #make directory, crea una carpeta llamada pfam y -p evita errores si la carpeta ya existe
 cd pfam #entra a la carpeta pfam
-wget https://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam38.2/Pfam-A.hmm.gz #descarga el archivo Pfam-A.hmm.gz desde el servidor FTP de EBI
+
+wget https://ftp.ebi.ac.uk/pub/databases/Pfam/releases/Pfam38.2/Pfam-A.hmm.gz #descarga el archivo Pfam-A.hmm.gz
+
 gunzip Pfam-A.hmm.gz #descomprime el archivo Pfam-A.hmm.gz
-hmmpress Pfam-A.hmm #prepara el archivo Pfam-A.hmm para su uso con HMMER, creando los archivos necesarios para la búsqueda de perfiles de HMM
+
+hmmfetch --index Pfam-A.hmm #crea un índice para el archivo Pfam-A.hmm, lo que permite buscar perfiles de HMM específicos dentro del archivo
+hmmfetch -f Pfam-A.hmm ../familias_pfam.txt > Pfam_reducido.hmm #extrae las familias de Pfam especificadas en el archivo familias_pfam.txt y guarda en Pfam_reducido.hmm
+
+hmmpress Pfam_reducido.hmm #prepara el archivo Pfam_reducido.hmm para su uso con HMMER, creando los archivos necesarios para la búsqueda de perfiles de HMM
+
+hmmscan --tblout resultados_pfam.tbl pfam/Pfam_reducido.hmm famUniProt/secuencias_uniprot.fasta
--- Download_famUniProt.sh
+++ Download_famUniProt.sh
@@ -1,4 +1,23 @@
-Download_famUniProt.sh
-mkdir famUniProt #make directory, crea una carpeta llamada famUniProt
-cd famUniProt #entra a la carpeta famUniProt
-wget "https://rest.uniprot.org/uniprotkb/P00533.fasta" -O secuencias_uniprot.fasta #descarga la secuencia de la proteína con el ID P00533 desde UniProt y la guarda en un archivo llamado secuencias_uniprot.fasta
+mkdir -p famUniProt #make directory, crea una carpeta llamada famUniProt para guardar las secuencias
+
+rm -f famUniProt/secuencias_uniprot.fasta #elimina el archivo secuencias_uniprot.fasta si ya existe, evitando errores al descargar nuevas secuencias
+
+while read id  #lee cada línea del archivo ids_uniprot.txt, que contiene los IDs de UniProt a descargar
+do
+    echo "Descargando: $id" #imprime un mensaje indicando el ID que se está descargando
+
+    wget "https://rest.uniprot.org/uniprotkb/${id}.fasta" -O famUniProt/temp.fasta #descarga la secuencia en formato FASTA para el ID actual y la guarda temporalmente en temp.fasta
+
+    if grep -q ">" famUniProt/temp.fasta #verifica si el archivo temp.fasta contiene una secuencia válida (buscando el símbolo ">" que indica el inicio de una secuencia FASTA)
+    then
+        cat famUniProt/temp.fasta >> famUniProt/secuencias_uniprot.fasta #si la secuencia es válida, se agrega al archivo secuencias_uniprot.fasta, acumulando todas las secuencias descargadas
+        echo "" >> famUniProt/secuencias_uniprot.fasta #agrega una línea en blanco después de cada secuencia para mejorar la legibilidad del archivo final
+        echo "Descarga exitosa: $id"  #imprime un mensaje indicando que la descarga fue exitosa para el ID actual
+    else
+        echo "No se pudo descargar: $id" #si el archivo temp.fasta no contiene una secuencia válida, se imprime un mensaje indicando que no se pudo descargar el ID actual
+    fi #elimina el archivo temporal temp.fasta para evitar confusiones en la próxima iteración
+
+done < ids_uniprot.txt #finaliza el ciclo, aquí se indica cual archivo va a leer
+
+fi #elimina el archivo temporal temp.fasta para evitar confusiones en la próxima iteración
+
